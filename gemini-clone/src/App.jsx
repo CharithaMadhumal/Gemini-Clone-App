38459694873c674548cd525591c4bc8125c9bcb0
CharithaMadhumal <charithamadhumal174@gmail.com>
2024-12-26 00:51:04 +0530

    starting UI part

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from './components/Sidebar/SideBar'
+
+const App = () => {
+  return (
+      <>
+        <SideBar/>
+      </>
+  )
+}
+
+export default App
