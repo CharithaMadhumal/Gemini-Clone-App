a2bfbb312de7552df0a5c7ffc54352c3a1e43119
CharithaMadhumal <charithamadhumal174@gmail.com>
2024-12-28 00:24:48 +0530

    completed

--- gemini-clone/src/App.jsx
+++ gemini-clone/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import SideBar from './components/Sidebar/SideBar'
+import Main from './components/Main/Main'
+
+
 
 const App = () => {
   return (
       <>
         <SideBar/>
+        <Main/>
+       
       </>
   )
 }
--- gemini-clone/src/components/Sidebar/SideBar.jsx
+++ gemini-clone/src/components/Sidebar/SideBar.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './SideBar.css'
 import {assets} from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const SideBar = () => {
 
     const [extended, setExtended] =useState(false)
+    const {onSent, prevPrompts, setRecentPrompt, newChat} = useContext(Context)
+
+    const loadPrompt = async (prompt) =>{
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
+
   return (
     <div className='sidebar'>
         <div className='top'>
-            <img className='menu' src={assets.menu_icon} alt=''/>
-            <div className='new-chat'>
+
+            <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt=''/>
+            <div onClick={()=>newChat()} className='new-chat'>
                 <img src={assets.plus_icon} alt=''/>
                 {extended?<p>New Chat</p>:null}
             </div>
@@ -17,10 +26,15 @@ const SideBar = () => {
             {extended ? 
                <div className='recent'>
                <p className='recent-title'>Recent</p>
-               <div className='recent-entry'>
-                   <img src={assets.message_icon} alt=''/>
-                   <p>What is react</p>
-               </div>
+               {prevPrompts.map((item,index)=>{
+                  return(
+                    <div onClick={()=>loadPrompt(item)} className='recent-entry'>
+                        <img src={assets.message_icon} alt=''/>
+                        <p>{item.slice(0,18)}...</p>
+                    </div>
+                  )
+               })}
+               
            </div>
            :null
         }
@@ -30,17 +44,17 @@ const SideBar = () => {
         <div className='bottom'>
             <div className='bottom-item recent-entry'>
                 <img src={assets.question_icon} alt=''/>
-                <p>Help</p>
+                {extended?<p>Help</p>:null}
             </div>
 
             <div className='bottom-item recent-entry'>
                 <img src={assets.history_icon} alt=''/>
-                <p>Activity</p>
+                {extended?<p>Activity</p>:null}
             </div>
 
             <div className='bottom-item recent-entry'>
                 <img src={assets.setting_icon} alt=''/>
-                <p>Settings</p>
+                {extended?<p>Settings</p>:null}
             </div>
         </div>
       
